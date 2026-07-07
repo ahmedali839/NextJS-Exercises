ad00a8a89eb5326b959b3197c15bf02095d17f05
Ahmed Yar <https://github.com/ahmedali839>
2026-07-07 10:50:46 +0500

    practising nextjs

--- installation_nextjs/app/page.js
+++ installation_nextjs/app/page.js
@@ -12,7 +12,7 @@ export default function Home() {
           height={20}
           priority
         />
-        // This is a test edit to verify Git is working
+        
         <div className="flex flex-col items-center gap-6 text-center sm:items-start sm:text-left">
           <h1 className="max-w-xs text-3xl font-semibold leading-10 tracking-tight text-black dark:text-zinc-50">
             To get started, edit the page.js file.
