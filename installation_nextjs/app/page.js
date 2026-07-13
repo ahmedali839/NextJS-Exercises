ca4543d821811d7bb26d3a7c6831aae88d10e9de
Ahmed Yar <https://github.com/ahmedali839>
2026-07-13 23:43:27 +0500

    adding new project

--- installation_nextjs/app/page.js
+++ installation_nextjs/app/page.js
@@ -5,7 +5,7 @@ export default function Home() {
     <div className="flex flex-col flex-1 items-center justify-center bg-zinc-50 font-sans dark:bg-black">
       <main className="flex flex-col items-center justify-center w-full flex-1 px-20 text-center">
         <h2 className="text-3xl font-bold text-zinc-800 dark:text-zinc-200">
-          Welcome to Next.js! // first project with Next.js and Tailwind CSS
+          Welcome to Next.js!
         </h2>
       </main>
     </div>
