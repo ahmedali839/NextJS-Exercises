4a8bf88bde3d7981573555c47066e6812a97affc
Ahmed Yar <ahmedyarr1212@gmail.com>
2026-07-21 23:56:30 +0500

    just basics

--- project_structure/app/page.js
+++ project_structure/app/page.js
@@ -18,10 +18,10 @@ export default function Home() {
           </h1>
           <p className="max-w-md text-lg leading-8 text-zinc-600 dark:text-zinc-400">
             Looking for a starting point or more instructions? Head over to{" "}
-            <a
+            {/* <a
               href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
               className="font-medium text-zinc-950 dark:text-zinc-50"
-            >
+            > */}
               Templates
             </a>
             or the
