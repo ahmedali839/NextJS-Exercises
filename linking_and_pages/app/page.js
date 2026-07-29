023d5e7dd48bd3356025596b675622f8813b9cb9
Ahmed Yar <ahmedyarr1212@gmail.com>
2026-07-29 23:52:39 +0500

    pages and layouts

--- linking_and_pages/app/page.js
+++ linking_and_pages/app/page.js
@@ -4,7 +4,7 @@ export default function Home() {
   return (
     <div className="flex flex-col flex-1 items-center justify-center bg-zinc-50 font-sans dark:bg-black">
       <main className="flex flex-1 w-full max-w-3xl flex-col items-center justify-between py-32 px-16 bg-white dark:bg-black sm:items-start">
-        {/* <Image
+        <Image
           className="dark:invert"
           src="/next.svg"
           alt="Next.js logo"
@@ -23,7 +23,7 @@ export default function Home() {
               className="font-medium text-zinc-950 dark:text-zinc-50"
             >
               Templates
-            </a>{" "} */}
+            </a>{" "}
             or the{" "}
             <a
               href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
